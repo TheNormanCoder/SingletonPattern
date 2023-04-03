0f40fb8fc9d2b331ec99bd5671b66a85f6dd0867
TheNormanCoder <n.billanova@gmail.com>
2023-04-03 10:11:14 +0200

    Delete src directory

--- src/ConcreteStrategyA.java
+++
@@ -1,5 +0,0 @@
-public class ConcreteStrategyA implements Strategy {
-    public void execute() {
-        System.out.println("Esecuzione dell'algoritmo A");
-    }
-}
--- src/ConcreteStrategyB.java
+++
@@ -1,5 +0,0 @@
-public class ConcreteStrategyB implements Strategy {
-    public void execute() {
-        System.out.println("Esecuzione dell'algoritmo B");
-    }
-}
